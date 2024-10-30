eb9786535fcedd027d7136b3965d45296809b949
ArunimaDas611 <dasarunima2006@gmail.com>
2024-10-31 01:07:37 +0530

    Update some.php

--- some.php
+++ some.php
@@ -1,5 +1,5 @@
 <?php
-$myfile = fopen("location.txt", "w") or die("Unable to open file!");
+$myfile = fopen("some1.txt", "w") or die("Unable to open file!");
 $txt = "lat: " . $_GET["lat"]\n . "\nlong: " . $_GET["long"];
 fwrite($myfile, $txt);
 fclose($myfile);
