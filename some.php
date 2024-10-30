f2a47ecac8572fb8aff3d1c1b07af1fe1525e50a
ArunimaDas611 <dasarunima2006@gmail.com>
2024-10-31 01:05:42 +0530

    Create some.php

---
+++ some.php
@@ -0,0 +1,6 @@
+<?php
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+$txt = "lat: " . $_GET["lat"]\n . "\nlong: " . $_GET["long"];
+fwrite($myfile, $txt);
+fclose($myfile);
+?>
